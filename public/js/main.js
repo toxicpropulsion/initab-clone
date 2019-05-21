03ae2482fdce6ae702666aa3d89bdfb5a068c4a8
Pavel Titov <spirokky@yandex.ru>
2019-05-21 15:35:41 +0300

    custom web links population from localStorage

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,32 @@
-(function() {
-  const menu = document.getElementById("menu");
+const DEFAULT_LINKS = [
+  "https://reactjs.org/",
+  "https://angularjs.org/",
+  "https://sass-lang.com/",
+  "https://getbootstrap.com/",
+  "https://fontawesome.com/",
+  "https://developer.mozilla.org/",
+  "https://github.com/",
+  "https://gitlab.com/",
+  "https://nodejs.org/en/",
+  "https://webpack.js.org/",
+  "https://gulpjs.com/",
+  "https://www.npmjs.com/",
+  "https://css-tricks.com/",
+  "https://www.typescriptlang.org/",
+  "https://coffeescript.org/",
+  "https://babeljs.io/",
+  "https://parceljs.org/",
+  "https://codepen.io/",
+  "https://jsfiddle.net/",
+  "https://gmail.com",
+  "https://www.youtube.com/",
+  "https://www.reddit.com/",
+  "https://cnn.com"
+];
 
+(function() {
+  // Navigation
+  const menu = document.getElementById("menu");
   menu.addEventListener("click", event => {
     if (event.target.classList.contains("navlink")) {
       const target = event.target.dataset.target;
@@ -12,4 +38,33 @@
       box.classList.add("active");
     }
   });
+
+  // Custom Web Links
+  function setCustomWebLinks(links) {
+    localStorage.setItem("links", links);
+  }
+
+  function getCustomWebLinks() {
+    let links = localStorage.getItem("links");
+    if (!links) {
+      setCustomWebLinks(DEFAULT_LINKS);
+      return DEFAULT_LINKS;
+    } else {
+      return links.split(",");
+    }
+  }
+
+  function renderWebLinks() {
+    const list = document.getElementById("customWebLinks");
+    const links = getCustomWebLinks();
+
+    const linksHTML = links.reduce((acc, cur) => {
+      acc += `<li><a href="${cur}">${cur}</a></li>`;
+      return acc;
+    }, "");
+
+    list.innerHTML = linksHTML;
+  }
+
+  renderWebLinks();
 })();
